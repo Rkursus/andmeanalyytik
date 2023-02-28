fb6b9fc6f55cb6adda538712f69a477d432c5bb7
Kaur Lumiste <44161463+klumiste@users.noreply.github.com>
2023-02-28 08:44:41 +0200

    teema2 update

--- _teema2/teema2_r_kood.R
+++ _teema2/teema2_r_kood.R
@@ -1,38 +1,60 @@
 # Andmeanalüütiku arenguprogramm
-# 7. õppepäev
+# 6. õppepäev
 # Statistilised meetodid praktilises elus
 #
 # Tuleviku prognoosimine
 # Autor: Kaur Lumiste
 
+install.packages("forecast")
+library(forecast)
+options(scipen=999) # R näitab murdarve komaga, mitte astmetega: 2.34e-2 -> 0.0234
+options(digits=3) # Ümardame kõik murdarvud tulemustes 3-kohta peale koma
 
-## Sissejuhatus aegridade analüüsi
+
+
+## Sissejuhatus aegridade analüüsi ----
 
 # Tavaline statistika aegridade puhul ei aita, sest aegridade puhul eiratakse 
 #  ühte põhilist eeldust statistikas - vaatluste sõtlumatus. 
 
+## Andmete ettevalmistus ----
 
-
-## Holt-Winters'i meetod
-
-# Proovime leida Holt-Winters’i meetodil Tallinki reisijate arvu prognoosi.
-load(url("http://www.ms.ut.ee/mart/andmeteadus/reisijaid.RData"))
+# Proovime leida prognoosi Tallinki reisijate arvu jaoks. 
+# Allikas: https://www.tallink.com/reports#tabs-content-6
+load(url("https://github.com/Rkursus/andmeanalyytik/raw/master/data/reisijaid.RData"))
 head(Tallink)
 attach(Tallink)
 
-
 # Teisendame andmed aegreaks:
-y=ts(reisijaid, start=c(2012,5), frequency=12)
+y = ts(reisijaid, start=c(2012,5), frequency=12)
 y
 plot(y)
+
+## Aegrea analüüsimine
+
+# Hindame Holt-Winters’i mudeli, selle meetodi abil saame hinnata: 
+#  * Autoregressiivsust (_alpha_) - kuidas mõjutab eelmine kuu käesolevat.
+#  * Trendi (_beta_) - kas ajas reisijate arv suureneb või väheneb stabiilselt?
+#  * sesoonsust (_gamma_) - kas eelmise aasta muster kordub aastast aastasse? Nt suvel on rohkem reisijaid, talvel vähem aga detsembris on jälle natuke suurem reisijate liikumine jõulude tõttu.
+#  * Iga üksiku kuu sesoonset mõju võrreldes keskmisega (_s1-s12_).
 
 # Hindame Holt-Winters’i mudeli
 m1=HoltWinters(y, seasonal="mult")
 m1
 
+# Aegrea komponendid joonisel:
+plot(stl(y, "per"), main = "Aegrea komponendid")
+
+# * data - algsed andmed ilma komponentideks lahku löömata.
+# * seasonal - sesoonsuse komponent, kuivõrd stabiilne on resisijate arvu muster aastast-aastasse.
+# * trend - kuidas on trend ajapikku muutunud.
+# * remainder - jääk, üldiselt kui "tulbad" jagunevad ühtlaselt, siis on komponentideks lahku löömine hästi õnnestunud.
+
+
+## Prognoosimine ----
+
 # Kontrollime mudeli prognoosivõimet – kui hästi ta on varem suutnud tulevikku näha?
 plot(m1)
-
 
 # Prognoosime leitud mudeli abil kaks aastat (24 kuud) tulevikku
 install.packages('forecast')
@@ -43,39 +65,43 @@ prog
 
 plot(prog, xlim=c(2012, 2020), main="Tallinki reisijate arvu prognoos")
 
-# Soovi korral võid mängida kunstnikku ja leida kõige meelepärasem graafik prognooside esitamiseks:
-prog = forecast(m1, h=24, level=0.95)
+### Ülesanne nuputamiseks
+
+# Millised järeldused saad teha vastavalt ülevel saadud joonisele? 
+# Kuivõrd täpne on prognoos? Millele see võib vihjata? 
+# Kuidas saab antud prognoosi kasutada ettevõtte igapäevases tegevuses?_
+
+
+## Prognoosi võrdlus tegelikkusega ----
+
+# Lisame graafikule prognoositud perioodi tegelikud reisijate arvud:
 plot(prog, xlim=c(2012, 2020), main="Tallinki reisijate arvu prognoos")
-
-prog = forecast(m1, h=24, level=c(0.5, 0.9, 0.95, 0.99))
-plot(prog, xlim=c(2012, 2020), main="Tallinki reisijate arvu prognoos", 
-  	shadecols=c("gray95", "gray85", "gray80", "gray75") )
-
-
-# Ning võid lisada graafikule ka hiljem lisandunud reisijate arvud:
-plot(prog, xlim=c(2012, 2020), main="Tallinki reisijate arvu prognoos", 
-  	shadecols=c("gray95", "gray85", "gray80", "gray75") )
 tegelik=c(789272, 852609, 989445, 1223901, 993078, 730631, 785583, 
-			654240, 811261, 549278, 620006, 686488, 779113)
+          654240, 811261, 549278, 620006, 686488, 779113,853309,1019421,
+          1238871,1017565,718354,800828,698293,781684,617374,669849)
 teg=ts(tegelik, start=c(2018, 4), frequency=12)
-points(teg, col="red", pch=20, cex=2)
+points(teg, col="red", cex=1.2, pch=20)
 
 
-# Võid vaadata ka, kuidas erinevad aegrea komponendid
-plot(fitted(m1))
-plot(stl(y, "per"))
+### Ülesanne nuputamiseks
 
+# Hinda Tallinki reisijate prognoosimudel uuesti kui `reisijaid` andmetele on juurde 
+#  pandud hiljem lisatud 2018-2022. aasta andmed (`tegelik`). Prognoosi järgmise 12 kuu 
+#  reisijate arvud.
 
-# ARIMA - mudelid
+# "Keevita" algsed andmed ja hiljem lisatud tegelikud andmed. Muuda aegreaks:
+y2 = ts(append(reisijaid, tegelik), start=c(2012,5), frequency=12)
 
-# On veel mudeleid, mille abil on võimalik aegrea struktuuri esitada, 
-#  nt ARIMA mudelid. 
-# ARIMA ehk AutoRegressive Integrated Moving Average
-# R-is on kindel abimees funktsioon `auto.arima()`
-y = ts(reisijaid, start=c(2012,5), frequency=12)
-m2 = auto.arima(y)
-prog = forecast(m2, h=10)
-plot(prog, xlim=c(2012, 2020), main="Tallinki reisijate arvu prognoos")
+# Joonista uus aegrida, lahuta komponentideks ja hinda komponente (trend, sesoonsus, juhuslik komponent)
+... sinu kood ...
+
+# Hinda Holt-Winters mudel
+... sinu kood ...
+
+# Prognoosi järgmised 12 kuud
+... sinu kood ...
+
+# Milliseid otsuseid teeksid kui näeksid sellist prognoosi? Kas ostaksid Tallinki aktsiaid?
 
 
 
